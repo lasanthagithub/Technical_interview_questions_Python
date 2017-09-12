4acdef4852b011a1faf39340ae03ce425192bb1a
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-12 17:50:47 -0400

    find_the_minimum_spanning_tree_within convert to graph

--- find_the_minimum_spanning_tree_within_1.py
+++ find_the_minimum_spanning_tree_within_1.py
@@ -2,11 +2,7 @@
 Question 3
 Given an undirected graph G, find the minimum spanning tree within G. 
 A minimum spanning tree connects all vertices in a graph with the smallest 
-possible total weight of edges. Your function should take in and return 
-an adjacency list structured like this:
-{'A': [('B', 2)],
- 'B': [('A', 2), ('C', 5)], 
- 'C': [('B', 5)]}
+possible total weight of edges. 
 Vertices are represented as unique strings. 
 The function definition should be question3(G)
 
@@ -16,232 +12,32 @@ which are connecting by an edge.
 An edge size is given as a 2-tuple with nodes , i.e. ("A","3")
 
 '''
-###############################################################################
+
+
 '''
-This part of codes aquired by Udacity course materials
+This function get in and return an adjacensy list tructure like this:
+{'A': [('B', 2)],
+ 'B': [('A', 2), ('C', 5)], 
+ 'C': [('B', 5)]}
 '''
-
-class Node(object):
-	def __init__(self, value):
-		self.value = value
-		self.edges = []
-
-class Edge(object):
-	def __init__(self, value, node_from, node_to):
-		self.value = value
-		self.node_from = node_from
-		self.node_to = node_to
-
-class Graph(object):
-	def __init__(self, nodes=[], edges=[]):
-		self.nodes = nodes
-		self.edges = edges
-
-	def insert_node(self, new_node_val):
-		new_node = Node(new_node_val)
-		self.nodes.append(new_node)
-	
-	def insert_edge(self, new_edge_val, node_from_val, node_to_val):
-		from_found = None
-		to_found = None
-		for node in self.nodes:
-			if node_from_val == node.value:
-				from_found = node
-			if node_to_val == node.value:
-				to_found = node
-		if from_found == None:
-			from_found = Node(node_from_val)
-			self.nodes.append(from_found)
-		if to_found == None:
-			to_found = Node(node_to_val)
-			self.nodes.append(to_found)
-		new_edge = Edge(new_edge_val, from_found, to_found)
-		from_found.edges.append(new_edge)
-		to_found.edges.append(new_edge)
-		self.edges.append(new_edge)
-
-	def get_edge_list(self):
-		edge_list = []
-		for edge_object in self.edges:
-			edge = (edge_object.value, edge_object.node_from.value, edge_object.node_to.value)
-			edge_list.append(edge)
-		return edge_list
-	
-	def get_adjacency_list(self):
-		max_index = self.find_max_index()
-		adjacency_list = [None] * (max_index + 1)
-		for edge_object in self.edges:
-			if adjacency_list[edge_object.node_from.value]:
-				adjacency_list[edge_object.node_from.value].append((edge_object.node_to.value, edge_object.value))
-			else:
-				adjacency_list[edge_object.node_from.value] = [(edge_object.node_to.value, edge_object.value)]
-		return adjacency_list
-	
-	def get_adjacency_matrix(self):
-		max_index = self.find_max_index()
-		adjacency_matrix = [[0 for i in range(max_index + 1)] for j in range(max_index + 1)]
-		for edge_object in self.edges:
-			adjacency_matrix[edge_object.node_from.value][edge_object.node_to.value] = edge_object.value
-		return adjacency_matrix
-	
-	def find_max_index(self):
-		max_index = -1
-		if len(self.nodes):
-			for node in self.nodes:
-				if node.value > max_index:
-					max_index = node.value
-		return max_index
-
-###############################################################################
-
-
-
-
-
-
-
-
-
-
-
-
 def question3(g):
 	## Asumptions: all the nodes 
 	g_key_list = g.keys()
 	#print(g_key_list)
 	
 	start_node = 'A'
+	
+	paths = {} ## {path1: [[A, B, C,D..], total_edge_val]}
+	
+	for node in g_key_list:
+		
+		for edge in g[node]:
+			to_node = edge[0]
+			print(to_node)
+		
 
-
-import sys
-
-class Vertex:
-    def __init__(self, node):
-        self.id = node
-        self.adjacent = {}
-        # Set distance to infinity for all nodes
-        self.distance = sys.maxint
-        # Mark all nodes unvisited        
-        self.visited = False  
-        # Predecessor
-        self.previous = None
-
-    def add_neighbor(self, neighbor, weight=0):
-        self.adjacent[neighbor] = weight
-
-    def get_connections(self):
-        return self.adjacent.keys()  
-
-    def get_id(self):
-        return self.id
-
-    def get_weight(self, neighbor):
-        return self.adjacent[neighbor]
-
-    def set_distance(self, dist):
-        self.distance = dist
-
-    def get_distance(self):
-        return self.distance
-
-    def set_previous(self, prev):
-        self.previous = prev
-
-    def set_visited(self):
-        self.visited = True
-
-    def __str__(self):
-        return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
-
-class Graph:
-    def __init__(self):
-        self.vert_dict = {}
-        self.num_vertices = 0
-
-    def __iter__(self):
-        return iter(self.vert_dict.values())
-
-    def add_vertex(self, node):
-        self.num_vertices = self.num_vertices + 1
-        new_vertex = Vertex(node)
-        self.vert_dict[node] = new_vertex
-        return new_vertex
-
-    def get_vertex(self, n):
-        if n in self.vert_dict:
-            return self.vert_dict[n]
-        else:
-            return None
-
-    def add_edge(self, frm, to, cost = 0):
-        if frm not in self.vert_dict:
-            self.add_vertex(frm)
-        if to not in self.vert_dict:
-            self.add_vertex(to)
-
-        self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
-        self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
-
-    def get_vertices(self):
-        return self.vert_dict.keys()
-
-    def set_previous(self, current):
-        self.previous = current
-
-    def get_previous(self, current):
-        return self.previous
-
-def shortest(v, path):
-    ''' make shortest path from v.previous'''
-    if v.previous:
-        path.append(v.previous.get_id())
-        shortest(v.previous, path)
-    
-
-import heapq
-
-def dijkstra(aGraph, start, target):
-    print ('''Dijkstra's shortest path''')
-    # Set the distance for the start node to zero 
-    start.set_distance(0)
-
-    # Put tuple pair into the priority queue
-    unvisited_queue = [(v.get_distance(),v) for v in aGraph]
-    heapq.heapify(unvisited_queue)
-
-    while len(unvisited_queue):
-        # Pops a vertex with the smallest distance 
-        uv = heapq.heappop(unvisited_queue)
-        current = uv[1]
-        current.set_visited()
-
-        #for next in v.adjacent:
-        for next in current.adjacent:
-            # if visited, skip
-            if next.visited:
-                continue
-            new_dist = current.get_distance() + current.get_weight(next)
-            
-            if new_dist < next.get_distance():
-                next.set_distance(new_dist)
-                next.set_previous(current)
-                print ('updated : current = %s next = %s new_dist = %s' \
-                        %(current.get_id(), next.get_id(), next.get_distance()))
-            else:
-                print ('not updated : current = %s next = %s new_dist = %s' \
-                        %(current.get_id(), next.get_id(), next.get_distance()))
-
-        # Rebuild heap
-        # 1. Pop every item
-        while len(unvisited_queue):
-            heapq.heappop(unvisited_queue)
-        # 2. Put all vertices not visited into the queue
-        unvisited_queue = [(v.get_distance(),v) for v in aGraph if not v.visited]
-        heapq.heapify(unvisited_queue)
-
-
-
-
+def convert_to_graph():
+	
 
 
 g = {'A' : [('B', 7), ('C', 9), ('F', 14)], 
@@ -249,10 +45,8 @@ g = {'A' : [('B', 7), ('C', 9), ('F', 14)],
      'C' : [('A', 9), ('B', 10), ('D', 11), ('F', 2)],
      'D' : [('B', 15), ('C', 11), ('E', 6)],
      'E' : [('D', 6), ('F', 9)],
-     'F' : [('A', 14, ('C', 2), ('E', 9))],
-					
-					}
-					
+     'F' : [('A', 14), ('C', 2), ('E', 9)]
+	}
+
 question3(g)
 
-dijkstra(g, 'A', 'F')
