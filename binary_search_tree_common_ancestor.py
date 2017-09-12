685633f5ac42fbcbb96f1c0dc807634f7c02cb3f
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-11 22:04:39 -0400

    find_the_minimum_spanning_tree_within

--- binary_search_tree_common_ancestor.py
+++ binary_search_tree_common_ancestor.py
@@ -9,4 +9,79 @@ an adjacency list structured like this:
  'C': [('B', 5)]}
 Vertices are represented as unique strings. 
 The function definition should be question3(G)
+
+The keys of the dictionary are the nodes of our graph. 
+The corresponding values are lists with the nodes, 
+which are connecting by an edge.
+An edge size is given as a 2-tuple with nodes , i.e. ("A","3")
+
 '''
+
+def question3(g):
+	## Asumptions: all the nodes 
+	g_key_list = g.keys()
+	#print(g_key_list)
+	
+	start_node = 'A'
+
+
+
+
+import heapq
+
+def dijkstra(aGraph, start, target):
+    print ('''Dijkstra's shortest path''')
+    # Set the distance for the start node to zero 
+    start.set_distance(0)
+
+    # Put tuple pair into the priority queue
+    unvisited_queue = [(v.get_distance(),v) for v in aGraph]
+    heapq.heapify(unvisited_queue)
+
+    while len(unvisited_queue):
+        # Pops a vertex with the smallest distance 
+        uv = heapq.heappop(unvisited_queue)
+        current = uv[1]
+        current.set_visited()
+
+        #for next in v.adjacent:
+        for next in current.adjacent:
+            # if visited, skip
+            if next.visited:
+                continue
+            new_dist = current.get_distance() + current.get_weight(next)
+            
+            if new_dist < next.get_distance():
+                next.set_distance(new_dist)
+                next.set_previous(current)
+                print ('updated : current = %s next = %s new_dist = %s' \
+                        %(current.get_id(), next.get_id(), next.get_distance()))
+            else:
+                print ('not updated : current = %s next = %s new_dist = %s' \
+                        %(current.get_id(), next.get_id(), next.get_distance()))
+
+        # Rebuild heap
+        # 1. Pop every item
+        while len(unvisited_queue):
+            heapq.heappop(unvisited_queue)
+        # 2. Put all vertices not visited into the queue
+        unvisited_queue = [(v.get_distance(),v) for v in aGraph if not v.visited]
+        heapq.heapify(unvisited_queue)
+
+
+
+
+
+
+g = {'A' : [('B', 7), ('C', 9), ('F', 14)], 
+     'B' : [('A', 7), ('C', 10), ('D', 15)], 
+     'C' : [('A', 9), ('B', 10), ('D', 11), ('F', 2)],
+     'D' : [('B', 15), ('C', 11), ('E', 6)],
+     'E' : [('D', 6), ('F', 9)],
+     'F' : [('A', 14, ('C', 2), ('E', 9))],
+					
+					}
+					
+question3(g)
+
+dijkstra(g, 'A', 'F')
