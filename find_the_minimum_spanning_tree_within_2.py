9a50feeecc3909c82651f02e690a7b8a62cb3c67
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-13 00:45:44 -0400

    find_the_minimum_spanning_tree_within: got paths

--- find_the_minimum_spanning_tree_within_2.py
+++ find_the_minimum_spanning_tree_within_2.py
@@ -12,79 +12,10 @@ which are connecting by an edge.
 An edge size is given as a 2-tuple with nodes , i.e. ("A","3")
 
 '''
-'''
-class Node(object):
-	def __init__(self, value):
-		self.value = value
-		self.edges = []
 
-class Edge(object):
-	def __init__(self, value, node_from, node_to):
-		self.value = value
-		self.node_from = node_from
-		self.node_to = node_to
-
-class Graph(object):
-	def __init__(self, nodes=[], edges=[]):
-		self.nodes = nodes
-		self.edges = edges
-
-	def insert_node(self, new_node_val):
-		new_node = Node(new_node_val)
-		self.nodes.append(new_node)
-	
-	def insert_edge(self, new_edge_val, node_from_val, node_to_val):
-		from_found = None
-		to_found = None
-		for node in self.nodes:
-			if node_from_val == node.value:
-				from_found = node
-			if node_to_val == node.value:
-				to_found = node
-		if from_found == None:
-			from_found = Node(node_from_val)
-			self.nodes.append(from_found)
-		if to_found == None:
-			to_found = Node(node_to_val)
-			self.nodes.append(to_found)
-		new_edge = Edge(new_edge_val, from_found, to_found)
-		from_found.edges.append(new_edge)
-		to_found.edges.append(new_edge)
-		self.edges.append(new_edge)
-
-	def get_edge_list(self):
-		edge_list = []
-		for edge_object in self.edges:
-			edge = (edge_object.value, edge_object.node_from.value, edge_object.node_to.value)
-			edge_list.append(edge)
-		return edge_list
-	
-	def get_adjacency_list(self):
-		max_index = self.find_max_index()
-		adjacency_list = [None] * (max_index + 1)
-		for edge_object in self.edges:
-			if adjacency_list[edge_object.node_from.value]:
-				adjacency_list[edge_object.node_from.value].append((edge_object.node_to.value, edge_object.value))
-			else:
-				adjacency_list[edge_object.node_from.value] = [(edge_object.node_to.value, edge_object.value)]
-		return adjacency_list
-	
-	def get_adjacency_matrix(self):
-		max_index = self.find_max_index()
-		adjacency_matrix = [[0 for i in range(max_index + 1)] for j in range(max_index + 1)]
-		for edge_object in self.edges:
-			adjacency_matrix[edge_object.node_from.value][edge_object.node_to.value] = edge_object.value
-		return adjacency_matrix
-	
-	def find_max_index(self):
-		max_index = -1
-		if len(self.nodes):
-			for node in self.nodes:
-				if node.value > max_index:
-					max_index = node.value
-		return max_index
-'''
 ###############################################################################
+'''Some of the parts of the classes in this block is aquired form Udacity 
+class notes '''
 class Node(object):
     def __init__(self, value):
         self.value = value
@@ -96,13 +27,6 @@ class Edge(object):
         self.value = value
         self.node_from = node_from
         self.node_to = node_to
-
-# You only need to change code with docs strings that have TODO.
-# Specifically: Graph.dfs_helper and Graph.bfs
-# New methods have been added to associate node numbers with names
-# Specifically: Graph.set_node_names
-# and the methods ending in "_names" which will print names instead
-# of node numbers
 
 class Graph(object):
     def __init__(self, nodes=None, edges=None):
@@ -219,15 +143,6 @@ class Graph(object):
             node.visited = False
 
     def dfs_helper(self, start_node):
-        """TODO: Write the helper function for a recursive implementation
-        of Depth First Search iterating through a node's edges. The
-        output should be a list of numbers corresponding to the
-        values of the traversed nodes.
-        ARGUMENTS: start_node is the starting Node
-        MODIFIES: the value of the visited property of nodes in self.nodes 
-        RETURN: a list of the traversed node values (integers).
-        """
-
         """The helper function for a recursive implementation
         of Depth First Search iterating through a node's edges. The
         output should be a list of numbers corresponding to the
@@ -246,7 +161,6 @@ class Graph(object):
                 ret_list.extend(self.dfs_helper(edge.node_to))
         return ret_list        
         
-        
 
     def dfs(self, start_node_num):
         """Outputs a list of numbers corresponding to the traversed nodes
@@ -257,21 +171,12 @@ class Graph(object):
         self._clear_visited()
         start_node = self.find_node(start_node_num)
         return self.dfs_helper(start_node)
-        
-        
-        
 
     def dfs_names(self, start_node_num):
         """Return the results of dfs with numbers converted to names."""
         return [self.node_names[num] for num in self.dfs(start_node_num)]
 
     def bfs(self, start_node_num):
-        """TODO: Create an iterative implementation of Breadth First Search
-        iterating through a node's edges. The output should be a list of
-        numbers corresponding to the traversed nodes.
-        ARGUMENTS: start_node_num is the node number (integer)
-        MODIFIES: the value of the visited property of nodes in self.nodes
-        RETURN: a list of the node values (integers)."""
         """An iterative implementation of Breadth First Search
         iterating through a node's edges. The output should be a list of
         numbers corresponding to the traversed nodes.
@@ -303,10 +208,10 @@ class Graph(object):
         """Return the results of bfs with numbers converted to names."""
         return [self.node_names[num] for num in self.bfs(start_node_num)]
 
-
+###############################################################################
 
 '''
-This function get in and return an adjacensy list tructure like this:
+This function get in and return an adjacensy list structure like this:
 {'A': [('B', 2)],
  'B': [('A', 2), ('C', 5)], 
  'C': [('B', 5)]}
@@ -314,23 +219,32 @@ This function get in and return an adjacensy list tructure like this:
 def question3(g):
 	## Asumptions: all the nodes 
 	g_key_list = list(g.keys())
-	#print(g_key_list)
-	
-
 	graph = Graph()
-
-	graph.set_node_names(g_key_list)  # 6 
+	
+	## set and convert node names to numerical values
+	graph.set_node_names(g_key_list)
 
 	## convert g to Graph. Insert edges and nodes
 	for node in g_key_list:
 		for edge in g[node]:
-			graph.insert_edge(int(edge[1]), g_key_list.index(node), g_key_list.index(edge[0]))
-	
+			graph.insert_edge(int(edge[1]), g_key_list.index(node),\
+			g_key_list.index(edge[0]))
+			
+			
 	## Get potential paths
-	paths = []
-	for i in range(len(g_key_list)):
-		paths.append(graph.bfs_names(i))
+			
+	def get_paths(paths_, nod_names, key_list):
+		for i in range(len(key_list)):
+			path = nod_names(i)
+			if path in paths or path[::-1] in paths: continue
+			paths_.append(path)
+		return paths_
 	
+	paths = []
+	paths = get_paths(paths, graph.bfs_names, g_key_list)
+	paths + get_paths(paths, graph.dfs_names, g_key_list)
+
+
 	
 	
 	
@@ -339,7 +253,10 @@ def question3(g):
 	
 	pp.pprint(paths)
 	print ("Edge List")
-	pp.pprint(graph.get_edge_list_names())	
+	#pp.pprint(graph.get_edge_list_names())
+
+
+	print()
 	
 	'''
 
@@ -374,6 +291,10 @@ g = {'A' : [('B', 7), ('C', 9), ('F', 14)],
      'E' : [('D', 6), ('F', 9)],
      'F' : [('A', 14), ('C', 2), ('E', 9)]
 	}
+
+g1 = {'A': [('B', 2)],
+ 'B': [('A', 2), ('C', 5)], 
+ 'C': [('B', 5)]}
 
 question3(g)
 
