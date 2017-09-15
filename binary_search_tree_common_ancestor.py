3aaab1ff164c2aee5209c24b13a56e5df663956b
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-15 01:54:18 -0400

    Binary search tree. find value

--- binary_search_tree_common_ancestor.py
+++ binary_search_tree_common_ancestor.py
@@ -78,115 +78,127 @@ print (tree.print_tree())
 '''
 
 class Node(object):
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
 
 class BST(object):
-    def __init__(self, root):
-        self.root = Node(root)
+	def __init__(self, root):
+		self.root = Node(root)
 
-    def insert(self, new_val):
-        self.insert_helper(self.root, new_val)
+	def insert(self, new_val):
+		self.insert_helper(self.root, new_val)
 
-    def insert_helper(self, current, new_val):
-        if current.value < new_val:
-            if current.right:
-                self.insert_helper(current.right, new_val)
-            else:
-                current.right = Node(new_val)
-        else:
-            if current.left:
-                self.insert_helper(current.left, new_val)
-            else:
-                current.left = Node(new_val)
+	def insert_helper(self, current, new_val):
+		if current.value < new_val:
+			if current.right:
+				self.insert_helper(current.right, new_val)
+			else:
+				current.right = Node(new_val)
+		else:
+			if current.left:
+				self.insert_helper(current.left, new_val)
+			else:
+				current.left = Node(new_val)
 
-    def search(self, find_val):
-        return self.search_helper(self.root, find_val)
-
-    def search_helper(self, current, find_val):
-        if current:
-            if current.value == find_val:
-                return True
-            elif current.value < find_val:
-                return self.search_helper(current.right, find_val)
-            else:
-                return self.search_helper(current.left, find_val)
-        return False
-
-'''
-# Set up tree
-tree = BST(4)
-
-# Insert elements
-tree.insert(2)
-tree.insert(1)
-tree.insert(3)
-tree.insert(5)
-
-# Check search
-# Should be True
-print (tree.search(4))
-# Should be False
-print (tree.search(6))
-
-'''
+	def search(self, find_val):
+		return self.search_helper(self.root, find_val)
 
 
+	def search_helper(self, current, find_val):
+		if current:
+			if current.value == find_val:
+				return True
+			elif current.value < find_val:
+				return self.search_helper(current.right, find_val)
+			else:
+				return self.search_helper(current.left, find_val)
+		return False
+		
+	def find_path_to(self, find_val):
+		return self.find_path_to_helper(self.root, find_val)
 
+	def find_path_to_helper(self, current, find_val, path = [], found = False):
+		
+		
+		if current:
+			if current.value == find_val:
+				#path.append(current.value)
+				found = True
+				
+			elif current.value < find_val:
+				path.append(current.right)
+				self.find_path_to_helper(current.right, find_val, path)
+			else:
+				path.append(current.left)
+				self.find_path_to_helper(current.left, find_val, path)
+				
+		if found:
+			return path
+		else:
+			return []
+		
+		
+		
 
+	def search1(self, find_val):
+		"""Return True if the value
+		is in the tree, return
+		False otherwise."""
+		return self.preorder_search(self.root, find_val)
 
+	def print_tree(self):
+		"""Print out all tree nodes
+		as they are visited in
+		a pre-order traversal."""
+		return self.preorder_print(self.root, "")[:-1]
+
+	def preorder_search(self, start, find_val):
+		"""Helper method - use this to create a 
+		recursive search solution."""
+		if start:
+			if start.value == find_val:
+				return True
+			else:
+				return self.preorder_search(start.left, find_val) or self.preorder_search(start.right, find_val)
+		return False
+
+	def preorder_print(self, start, traversal):
+		"""Helper method - use this to create a 
+		recursive print solution."""
+		if start:
+			traversal += (str(start.value) + "-")
+			traversal = self.preorder_print(start.left, traversal)
+			traversal = self.preorder_print(start.right, traversal)
+		return traversal
 
 
 
 
 def question4(T, r, n1, n2):
-	## Enter root into tree
+	## Initialize and enter root into tree
 	tree = BST(r)
-	## Inser data in to BST
-	'''for num, lst in enumerate(T):
-		
-		for j, child in enumerate(lst):
-			if child == 1:
-				tree.insert(num)
-	
-	def get_children(T, node_lst, children = []):
-		order = []
-		
-		for child in node_lst:
-			if child == 1:
-				order.append(child)
-				children.append(child)
-				get_children(T, T[child], children)
-		return order
-	print(get_children(T, T[r]))
-	
-	'''
-	end =True
-	node = r
-	children = []
+
+	## Get the list of child nodes
 	def get_children(T, node, temp_lst = []):
 		for nod, child in enumerate(T[node]):
 			if child == 1:
 				temp_lst.append(nod)
 		return temp_lst
-		
-	children = get_children(T, r, children)
-	#print(children)
 	
+	children = get_children(T, r)
+	
+	## Inser data in to BST
 	while children:
 		temp_child = []
 		for sub_nod in children:
 			tree.insert(sub_nod)
-
-			'''for nod, child in enumerate(T[sub_nod]):
-				if child == 1:
-					temp_child.append(nod)
-			'''
 			temp_child = get_children(T, sub_nod, temp_child)
 		children = temp_child
-		#print(children)
+	
+	print(tree.find_path_to(3))
+	
 	print (tree.search(0))
 	print (tree.search(1))
 	print (tree.search(2))
@@ -195,8 +207,12 @@ def question4(T, r, n1, n2):
 	print (tree.search(5))
 	print (tree.search(6))
 	print()
+	
+	#print(tree.print_tree())
 
 
+## Test cases
+'''
 question4([[0, 1, 0, 0, 0],
            [0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0],
@@ -216,6 +232,7 @@ question4([[0, 0, 0, 0, 0],
            [0, 1, 0, 1, 0],
            [0, 0, 0, 0, 1],
            [0, 0, 0, 0, 0]],2, 1, 2)
+'''
 
 question4([[0, 0, 0, 0, 0, 0, 0],
            [1, 0, 0, 0, 0, 0, 0],
